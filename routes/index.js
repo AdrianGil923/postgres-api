5b3900bff9d0f11f79f8916183ddf05aeba42df1
AdrianGil923 <zS23004688@estuadiantes.uv.mx>
2025-04-22 22:06:28 -0600

    Actualizacion

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 const { Router } = require('express');
-const controllers = require( '../controllers');
+const controllers = require('../controllers');
 
 const router = Router();
 
-router.get('/', (req, res) => res.send('Welcome'));
-
 router.post('/users', controllers.createUser);
 router.get('/users', controllers.getAllUsers);
+router.get('/users/:id', controllers.getUserById); 
+router.delete('/users/:id', controllers.deleteUser);
+router.put('/users/:id', controllers.updateUser);
 
 module.exports = router;
-
